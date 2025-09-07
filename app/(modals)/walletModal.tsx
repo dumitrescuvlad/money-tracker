932a2af7be07310ff280ab7cba3b280710e9cf03
dumitrescuvlad <emiyazwei@gmail.com>
2025-09-07 18:42:04 +0200

    wallet, need to work on fetch and update wallet

--- app/(modals)/walletModal.tsx
+++ app/(modals)/walletModal.tsx
@@ -7,10 +7,9 @@ import ModalWrapper from "@/components/ModalWrapper";
 import Typo from "@/components/Typo";
 import { colors, spacingX, spacingY } from "@/constants/theme";
 import { useAuth } from "@/contexts/authContexts";
-import { updateUser } from "@/services/userService";
+import { createOrUpdateWallet } from "@/services/walletServices";
 import { WalletType } from "@/types";
 import { scale, verticalScale } from "@/utils/styling";
-import * as ImagePicker from "expo-image-picker";
 import { useRouter } from "expo-router";
 import React, { useState } from "react";
 import { Alert, ScrollView, StyleSheet, View } from "react-native";
@@ -23,35 +22,27 @@ const WalletModal = () => {
   const [loading, setLoading] = useState(false);
   const router = useRouter();
 
-  const onPickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ["images"],
-      //allowsEditing: true,
-      aspect: [4, 3],
-      quality: 0.5,
-    });
-
-    if (!result.canceled) {
-      // setUserData({ ...userData, image: result.assets[0] });
-    }
-  };
-
   const onSubmit = async () => {
     let { name, image } = wallet;
     if (!name.trim() || !image) {
-      Alert.alert("User", "Please fill all the fields");
+      Alert.alert("Wallet", "Please fill all the fields");
       return;
     }
 
+    const data: WalletType = {
+      name,
+      image,
+      uid: user?.uid,
+    };
+    // include wallet id if updating
     setLoading(true);
-    const res = await updateUser(user?.uid as string, wallet);
+    const res = await createOrUpdateWallet(data);
     setLoading(false);
+    console.log("result:", res);
     if (res.success) {
-      // update user
-      updateUserData(user?.uid as string);
       router.back();
     } else {
-      Alert.alert("User", res.msg);
+      Alert.alert("Wallet", res.msg);
     }
   };
   return (
